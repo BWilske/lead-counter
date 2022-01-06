8728181ed6a90c7c0fa2cbfcc06804a868bdc2c5
Brent Wilske <wilskeworkflow@gmail.com>
2022-01-06 10:17:29 -0800

    Added functionality to allow the leads to persist across sessions using local storage

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const inputBtn= document.querySelector("#input-btn")
 const inputEl= document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 let myLeads = []
+pullLeadsLocalStorage()
 
-//alows submission
+//alows submission of leads
 
 inputBtn.addEventListener("click", function () {
-    let activelead = inputEl.value
-    myLeads.push(activelead)
+    myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
     displayLeads()
 })
 //handles displaying the links to the page
@@ -16,7 +17,6 @@ inputBtn.addEventListener("click", function () {
 function displayLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-    // listItems  += "<li><a href='" +myLeads[i] + "' target=_blank>" + myLeads[i] + "</a></li>"
     listItems  += `
     <li>
         <a href='${myLeads[i]} target='_blank'> 
@@ -29,14 +29,6 @@ function displayLeads() {
 }
 // presses the submit button when enter is pressed
 
-// function enterSubmit(event) {
-//     event.preventDefault();
-//     if (event.keycode === 13) {
-//         console.log("this is fine")
-//         inputBtn.click();
-//     }
-// }
-
     inputEl.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
@@ -44,3 +36,10 @@ function displayLeads() {
 
     }
 });
+
+ function pullLeadsLocalStorage() {
+    let leadstorage = JSON.parse(localStorage.getItem("myLeads"))
+    if (leadstorage !== null)
+        myLeads = leadstorage
+        displayLeads()
+ }
